0f5576735cdef32b24dee630765baf07fece0023
callum <callumbloys7@gmail.com>
2022-01-29 00:51:30 +0000

    Added Graphics settings and resolution settings

--- GGJ_2022_Creative_TN/Assets/Scripts/Managers/UIManager.cs
+++ GGJ_2022_Creative_TN/Assets/Scripts/Managers/UIManager.cs
@@ -30,12 +30,21 @@ public class UIManager : Singleton<UIManager>
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void StartFunction()
@@ -96,5 +105,10 @@ public class UIManager : Singleton<UIManager>
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
