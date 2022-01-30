ac17da48fb9738779a87029ec19e9ae00fdcc1e5
Sam Stamp <samstamp6@gmail.com>
2022-01-30 01:07:20 +0000

    Updated Main Menu & Created Link Between Scenes

--- GGJ_2022_Creative_TN/Assets/Scripts/UI/TutorialNotification.cs
+++ GGJ_2022_Creative_TN/Assets/Scripts/UI/TutorialNotification.cs
@@ -1,13 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ButtonTransition : MonoBehaviour
+public class TutorialNotification : MonoBehaviour
 {
-    public Color wantedcolour;
-    public Text buttonText;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +13,6 @@ public class ButtonTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    }
-
-    public void ChangeButtonColour()
-    {
-        buttonText.color = wantedcolour;
+        
     }
 }
